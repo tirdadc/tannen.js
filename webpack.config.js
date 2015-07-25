47d3a066a74fb82f0b49a91b902032ad5c01fd0f
tirdadc <tirdadck@gmail.com>
2015-07-25 13:43:03 -0400

    Now properly compiles .scss to .css

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,28 @@
 var path = require('path');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
-  entry: path.resolve(__dirname, 'src/Tannen.js.jsx'),
+  entry: path.resolve(__dirname, 'stylesheets/tannen.css.scss'),
   output: {
-    path: 'dist'
+    path: 'dist',
+    filename: 'tannen.css'
   },
   module: {
     loaders: [
       {
         test: /\.scss$/,
-        loader: 'style!css!sass'
+        loader: ExtractTextPlugin.extract(
+          "style",
+          "css!sass"
+        )
       },
       {
         test: /\.jsx$/,
         loader: 'jsx'
       },
     ]
-  }
+  },
+  plugins: [
+    new ExtractTextPlugin("tannen.css")
+  ]
 };
