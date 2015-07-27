a3931a394edc3cfcc0d5c7c2d23eb6acfd175710
tirdadc <tirdadck@gmail.com>
2015-07-27 13:14:18 -0400

    Added some events to example

--- app/main.jsx
+++ app/main.jsx
@@ -2,6 +2,28 @@ var React = require('react')
   , Tannen = require('../src/Tannen');
 
 React.render(
-  <Tannen />,
+  <Tannen
+    events={[
+      {
+        start_date: '2015-07-22',
+        end_date: '2015-07-28',
+        title: 'Bully George McFly'
+      },
+      {
+        start_date: '2015-07-24',
+        end_date: '2015-07-24',
+        title: 'Hang out at Cafe 80\'s'
+      },
+      {
+        start_date: '2015-07-05',
+        end_date: '2015-07-10',
+        title: 'Recover almanac'
+      },
+      {
+        start_date: '2015-07-24',
+        end_date: '2015-07-27',
+        title: 'Borrow DeLorean'
+      }
+    ]}/>,
   document.getElementById('tannen-example')
 );
